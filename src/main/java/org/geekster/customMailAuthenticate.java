57bb063073943cf2468465b1c1e315b3f8e3215e
Tanisht4869 <hellsin4869@gmail.com>
2023-06-11 14:53:05 +0530

    Java api automation prototype

---
+++ src/main/java/org/geekster/customMailAuthenticate.java
@@ -0,0 +1,11 @@
+package org.geekster;
+
+import javax.mail.PasswordAuthentication;
+import javax.mail.Authenticator;
+
+public class customMailAuthenticate extends Authenticator {
+    protected PasswordAuthentication getPasswordAuthentication()
+    {
+        return new PasswordAuthentication(mailMetaData.senderMail,mailMetaData.password);
+    }
+}
